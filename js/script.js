2c3a5a38103fc3107e65f614b04f23080c4d2143
xdimit <xdimit@rambler.ru>
2020-11-22 03:54:49 +0500

    Task: Print even numbers

--- js/script.js
+++ js/script.js
@@ -1,19 +1,7 @@
 	'use strict'
 
 
-	let log = prompt("Enter login: ", "");
-
-	if (log === null || log === "") {
-	    alert("Отмена");
-	} else if (log !== "Админ") {
-	    alert("Я вас не знаю");
-	} else {
-	    let pas = prompt("Enter password: ", "");
-	    if (pas === null || pas === "") {
-	        alert("Отмена");
-	    } else if (pas !== "Я главный") {
-	        alert("Неверный пароль");
-	    } else {
-	        alert("Здравствуйте!");
-	    }
-	};
+	for (let i = 2; i <= 10; i++) {
+		if (i % 2) continue;
+		alert(i);
+	}
