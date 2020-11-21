4cbb157c0ab2636acef46461030a1c1fa6dd7fc5
xdimit <xdimit@rambler.ru>
2020-11-22 03:31:20 +0500

    Task: Login verification

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-'use strict'
+	'use strict'
 
+
+	let log = prompt("Enter login: ", "");
+
+	if (log === null || log === "") {
+	    alert("Отмена");
+	} else if (log !== "Админ") {
+	    alert("Я вас не знаю");
+	} else {
+	    let pas = prompt("Enter password: ", "");
+	    if (pas === null || pas === "") {
+	        alert("Отмена");
+	    } else if (pas !== "Я главный") {
+	        alert("Неверный пароль");
+	    } else {
+	        alert("Здравствуйте!");
+	    }
+	};
