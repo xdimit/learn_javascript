e67306136614ad913c0c63a7e090910036f189e1
xdimit <xdimit@rambler.ru>
2020-11-23 22:45:50 +0500

    Task: Chain of calls

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
-	'use strict'
+'use strict'
 
-	let calculator = {
-	    read() {
-	        this.num1 = +prompt("Enter num1: ", "");
-	        this.num2 = +prompt("Enter num2: ", "");
-	    },
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep: function() { // показывает текущую ступеньку
+        alert(this.step);
+    }
+};
 
-	    sum() {
-	    	return this.num1 + this.num2;
-	    },
 
-	    mul() {
-	    	return this.num1 * this.num2;	
-	    },
-	};
-
-	calculator.read();
-	alert(calculator.sum());
-	alert(calculator.mul());
+ladder.up().up().down().showStep();
