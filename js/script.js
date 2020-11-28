e749a4bb4ffc8fab48f131bdbc48a5ebcdbd1e6b
xdimit <xdimit@rambler.ru>
2020-11-29 03:46:10 +0500

    Task: Convert text like border-left-width to borderLeftWidth

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
 'use strict'
 
-function sumInput() {
-    let numbers = [];
-    let sum = 0;
+// function camelize(str) {
+//     let arr = str.split('-');
+//     let newArr = [];
+//     newArr.push(arr[0]);
+//     for (let i = 1; i < arr.length; i++) {
 
-    while (true) {
+//         let string = arr[i];
 
-        let value = prompt("Enter num: ", "");
+//         let newString = string[0].toUpperCase() + string.slice(1);
+//         newArr.push(newString);
+//     }
+//     return newArr.join('');
+// }
 
-        if (!isFinite(value) || value === "" || value === null) break;
-
-        numbers.push(+value);
-    }
-
-    for (let num of numbers) {
-        sum += num;
-    }
-
-    return sum;
+function camelize(str) {
+    return str
+    .split('-')
+    .map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1))
+    .join('');
 }
 
-console.log(sumInput());
+console.log(camelize("background-color"));
+console.log(camelize("list-style-image"));
+console.log(camelize("-webkit-transition"));
