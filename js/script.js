f932363579c2a5d80297bec3c8d16f4d04841e78
xdimit <xdimit@rambler.ru>
2020-12-01 08:56:52 +0500

    Task: Range filtering

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
 'use strict'
 
-function getMaxSubSum(arr) {
-    let maxSum = 0;
-    let partialSum = 0
-    for (let i = 0; i < arr.length; i++) {
-    	partialSum += arr[i];
-    	maxSum = Math.max(maxSum, partialSum);
-    	if (partialSum < 0) partialSum = 0;
+function filterRange(arr, a, b) {
 
-    }
-    return maxSum;
+   return arr.filter((item) => (a <= item && item <= b) );
 }
 
-console.log(getMaxSubSum([-1, 2, 3, -9]));
-console.log(getMaxSubSum([2, -1, 2, 3, -9]));
-console.log(getMaxSubSum([-1, 2, 3, -9, 11]));
-console.log(getMaxSubSum([-2, -1, 1, 2]));
-console.log(getMaxSubSum([100, -9, 2, -3, 5]));
-console.log(getMaxSubSum([1, 2, 3]));
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert(filtered);
+
+alert(arr);
