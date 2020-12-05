b544fb901f30d76aebb8c6554d413c6985c5ea38
xdimit <xdimit@rambler.ru>
2020-12-05 09:26:28 +0500

    Task: Last day of the month?

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
 'use strict'
 
-function getDateAgo(date, days) {
-    let d = new Date(date);
-
-    return new Date(d.setDate(d.getDate() - days)).getDate();
+function getLastDayOfMonth(year, month) {
+    return new Date(year, month + 1, 0).getDate();
 }
 
-let date = new Date(2015, 0, 2);
-
-alert(getDateAgo(date, 1)); // 1, (1 Jan 2015)
-alert(getDateAgo(date, 2)); // 31, (31 Dec 2014)
-alert(getDateAgo(date, 365)); // 2, (2 Jan 2014)
+console.log(getLastDayOfMonth(2012, 1)); // 29 feb
