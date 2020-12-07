93330935b9cc83dccf9e4ee4a31e877f8e06180d
xdimit <xdimit@rambler.ru>
2020-12-07 07:35:31 +0500

    Task: Output every second

--- js/script.js
+++ js/script.js
@@ -1,22 +1,17 @@
 'use strict'
 
-function sum(a) {
-    let sum = a;
+function printNumbers(from, to) {
 
-    function f(b) {
-         sum += b;
-         return f;
-    }
+    let num = from;
 
-    f[Symbol.toPrimitive] = function() {
-            return sum;
+    setTimeout(function f() {
+        alert(num);
+        if (num < to) {
+            setTimeout(f, 1000);
         }
+        num++;
+    }, 1000);
 
-    return f;
 }
 
-console.log(sum(1)(2)); // == 3; // 1 + 2
-console.log(sum(1)(2)(3)); // == 6; // 1 + 2 + 3
-console.log(sum(5)(-1)(2)); // == 6
-console.log(sum(6)(-1)(-2)(-3)); // == 0
-console.log(sum(0)(1)(2)(3)(4)(5)); // == 15
+printNumbers(5, 10);
