7fcef0329bf7973ddb50f48c67dbbb9ae4203da9
xdimit <xdimit@rambler.ru>
2020-12-10 00:07:33 +0500

    Task: Using a Partially Applied Login Function

--- js/script.js
+++ js/script.js
@@ -1,43 +1,17 @@
 'use strict'
 
-function throttle(f, ms) {
-    let isCooldown = false,
-        saveArgs,
-        saveThis;
+function askPassword(ok, fail) {
+    let password = prompt("Password?", '');
+    if (password == "rockstar") ok();
+    else fail();
+}
 
-    return function wrapper(...args) {
-        if (isCooldown) {
-            saveArgs = args;
-            saveThis = this;
-            return;
-        }
+let user = {
+    name: 'John',
 
-        f.apply(this, args);
-
-        isCooldown = true;
-
-        setTimeout(() => {
-            isCooldown = false;
-            if (saveArgs) {
-                wrapper.apply(saveThis, saveArgs);
-                saveArgs = saveThis = null;
-            }
-        }, ms);
+    login(result) {
+        alert(this.name + (result ? ' logged in' : ' failed to log in'));
     }
+};
 
-    return wrapper;
-}
-
-function f(a) {
-    console.log(a)
-}
-
-// f1000 передаёт вызовы f максимум раз в 1000 мс
-let f1000 = throttle(f, 1000);
-
-f1000(1); // показывает 1
-f1000(2); // (ограничение, 1000 мс ещё нет)
-f1000(3); // (ограничение, 1000 мс ещё нет)
-
-// когда 1000 мс истекли ...
-// ...выводим 3, промежуточное значение 2 было проигнорировано
+askPassword(user.login.bind(user, true),user.login.bind(user, false)); // ?
