e820b743bf8b870d22d1d1adb29bfb4c74918dc3
xdimit <xdimit@rambler.ru>
2020-12-10 22:09:11 +0500

    Task: Add the 'defer ()' decorating method to the functions

--- js/script.js
+++ js/script.js
@@ -2,11 +2,20 @@
 
 Function.prototype.defer = function(ms) {
 
-    setTimeout(this, ms);
+    return (...args) => {
+        setTimeout(() => this.apply(null, args), ms);
+    }
 }
 
-function f() {
-  alert("Hello!");
+function f(a, b) {
+    alert(a + b);
 }
 
-f.defer(5000); // выведет "Hello!" через 1 секунду
+console.log(f.defer(5000)(1, 2)); // выведет 3 через 1 секунду.
+
+// Function.prototype.defer = function(ms) {
+//   let f = this;
+//   return function(...args) {
+//     setTimeout(() => f.apply(this, args), ms);
+//   }
+// };
