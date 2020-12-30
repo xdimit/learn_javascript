fc0efcd0aa2824a71ea7ef12798ad7be0b294bf7
xdimit <xdimit@rambler.ru>
2020-12-31 01:07:01 +0500

    Task: Inheriting from SyntaxError

--- js/script.js
+++ js/script.js
@@ -1,55 +1,17 @@
 'use strict'
 
-class Clock {
-    constructor({ template }) {
-        this.template = template;
-    }
-
-    render() {
-        let date = new Date();
-
-        let hours = date.getHours();
-        if (hours < 10) hours = '0' + hours;
-
-        let mins = date.getMinutes();
-        if (mins < 10) mins = '0' + mins;
-
-        let secs = date.getSeconds();
-        if (secs < 10) secs = '0' + secs;
-
-        let output = this.template
-            .replace('h', hours)
-            .replace('m', mins)
-            .replace('s', secs);
-
-        console.log(output);
-    }
-
-    stop() {
-        clearInterval(this.timer);
-    }
-
-    start() {
-        this.render();
-        this.timer = setInterval(() => this.render(), 1000);
+class FormatError extends SyntaxError {
+    constructor(message) {
+        super(message);
+        this.name = "FormatError";
     }
 }
 
-class ExtendedClock extends Clock {
-    constructor(options) {
-        super(options);
-        let { precision = 1000 } = options;
-        this.precision = precision;
-    }
+let err = new FormatError("ошибка форматирования");
 
-    start() {
-        this.render();
-        this.timer = setInterval(() => this.render(), this.precision);
-    }
-}
+alert( err.message ); // ошибка форматирования
+alert( err.name ); // FormatError
+alert( err.stack ); // stack
 
-let clock = new ExtendedClock({
-    template: 'h:m:s',
-    precision: 10000
-});
-clock.start();
+alert( err instanceof FormatError ); // true
+alert( err instanceof SyntaxError ); // true (потому что наследует от SyntaxError)
